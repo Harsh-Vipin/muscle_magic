b7dc259a0c185a3a66e00ffd770f22af668586fb
harshh-12 <harsh.vipinkp@gmail.com>
2022-10-30 15:07:13 +0530

    api

--- lib/networking/fetch_api.dart
+++ lib/networking/fetch_api.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-import 'package:muscle_magic/networking/apiKeys.dart';
 
 import 'package:muscle_magic/auth/secrets.dart';
 
@@ -11,9 +10,7 @@ Future fetchAPI() async {
   Uri uri = Uri.parse("https://exercisedb.p.rapidapi.com/exercises");
   Map<String, String> headers = {
 
-     "X-RapidAPI-Key": x_Rapid_API_Key,
-     "X-RapidAPI-Host": x_Rapid_API_Host,
-
+    
     "X-RapidAPI-Key": apikey,
     "X-RapidAPI-Host": 'exercisedb.p.rapidapi.com',
 
